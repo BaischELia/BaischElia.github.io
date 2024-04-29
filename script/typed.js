7cf505be57d6a0bdcca8a1ad08611c5db30d15da
Elia xx <eliamerlin@gmx.de>
2024-04-29 15:59:25 +0200

    Upload Portfolio

---
+++ script/typed.js
@@ -0,0 +1,6 @@
+let typed = new Typed('.typing', {
+    strings: ['Elia Baisch','Web Developer', 'UI/UX Designer', 'Application Developer', 'Graphic Designer', 'Visual Designer', 'Project Manager', 'Concept Developer'],
+    typeSpeed: 100,
+     backSpeed: 100,
+     loop: true
+    });
