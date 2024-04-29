f694b5ff2e17f9409f7dc07cf6de2361b6d8795a
Elia Baisch <141215960+BaischElia@users.noreply.github.com>
2024-04-29 12:15:15 +0200

    Delete script directory

--- script/typed.js
+++
@@ -1,6 +0,0 @@
-let typed = new Typed('.typing', {
-    strings: ['Elia Baisch','Web Developer', 'UI/UX Designer', 'Application Developer', 'Graphic Designer', 'Media Designer', 'Visual Designer'],
-    typeSpeed: 100,
-     backSpeed: 100,
-     loop: true
-    });
